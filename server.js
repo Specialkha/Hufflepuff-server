7a003acb92a28a82cdd763afd15f4da35bc80355
Pascal <pascal.bertrand85@laposte.net>
2021-02-08 08:28:49 +0100

    added changes on httpService

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var bodyParser = require("body-parser");
 var mongodb = require("mongodb");
 var ObjectID = mongodb.ObjectID;
 
-var CONTACTS_COLLECTION = "contacts";
+var USERS_COLLECTION = "users";
 
 var app = express();
 app.use(bodyParser.json());
@@ -18,7 +18,7 @@ app.use(express.static(distDir));
 var db;
 
 // Connect to the database before starting the application server.
-mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/test", {
+mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/CUBES", {
     useUnifiedTopology: true
 }, function (err, client) {
     if (err) {
@@ -37,7 +37,7 @@ mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:2701
     });
 });
 
-// CONTACTS API ROUTES BELOW
+// users API ROUTES BELOW
 
 // Generic error handler used by all endpoints.
 function handleError(res, reason, message, code) {
@@ -45,29 +45,29 @@ function handleError(res, reason, message, code) {
     res.status(code || 500).json({ "error": message });
 }
 
-/*  "/api/contacts"
- *    GET: finds all contacts
+/*  "/api/users"
+ *    GET: finds all users
  *    POST: creates a new contact
  */
 
-app.get("/api/contacts", function (req, res) {
-    db.collection(CONTACTS_COLLECTION).find({}).toArray(function (err, docs) {
+app.get("/api/" + USERS_COLLECTION, function (req, res) {
+    db.collection(USERS_COLLECTION).find({}).toArray(function (err, docs) {
         if (err) {
-            handleError(res, err.message, "Failed to get contacts.");
+            handleError(res, err.message, "Failed to get users.");
         } else {
             res.status(200).json(docs);
         }
     });
 });
 
-app.post("/api/contacts", function (req, res) {
+app.post("/api/" + USERS_COLLECTION, function (req, res) {
     var newContact = req.body;
     newContact.createDate = new Date();
 
     if (!req.body.name) {
         handleError(res, "Invalid user input", "Must provide a name.", 400);
     } else {
-        db.collection(CONTACTS_COLLECTION).insertOne(newContact, function (err, doc) {
+        db.collection(USERS_COLLECTION).insertOne(newContact, function (err, doc) {
             if (err) {
                 handleError(res, err.message, "Failed to create new contact.");
             } else {
@@ -77,14 +77,14 @@ app.post("/api/contacts", function (req, res) {
     }
 });
 
-/*  "/api/contacts/:id"
+/*  "/api/users/:id"
  *    GET: find contact by id
  *    PUT: update contact by id
  *    DELETE: deletes contact by id
  */
 
-app.get("/api/contacts/:id", function (req, res) {
-    db.collection(CONTACTS_COLLECTION).findOne({ _id: new ObjectID(req.params.id) }, function (err, doc) {
+app.get("/api/" + USERS_COLLECTION + "/:id", function (req, res) {
+    db.collection(USERS_COLLECTION).findOne({ _id: new ObjectID(req.params.id) }, function (err, doc) {
         if (err) {
             handleError(res, err.message, "Failed to get contact");
         } else {
@@ -93,11 +93,11 @@ app.get("/api/contacts/:id", function (req, res) {
     });
 });
 
-app.put("/api/contacts/:id", function (req, res) {
+app.put("/api/" + USERS_COLLECTION + "/:id", function (req, res) {
     var updateDoc = req.body;
     delete updateDoc._id;
 
-    db.collection(CONTACTS_COLLECTION).updateOne({ _id: new ObjectID(req.params.id) }, updateDoc, function (err, doc) {
+    db.collection(USERS_COLLECTION).updateOne({ _id: new ObjectID(req.params.id) }, updateDoc, function (err, doc) {
         if (err) {
             handleError(res, err.message, "Failed to update contact");
         } else {
@@ -107,8 +107,8 @@ app.put("/api/contacts/:id", function (req, res) {
     });
 });
 
-app.delete("/api/contacts/:id", function (req, res) {
-    db.collection(CONTACTS_COLLECTION).deleteOne({ _id: new ObjectID(req.params.id) }, function (err, result) {
+app.delete("/api/" + USERS_COLLECTION + "/:id", function (req, res) {
+    db.collection(USERS_COLLECTION).deleteOne({ _id: new ObjectID(req.params.id) }, function (err, result) {
         if (err) {
             handleError(res, err.message, "Failed to delete contact");
         } else {
--- src/app/core/http/http.service.ts
+++ src/app/core/http/http.service.ts
@@ -1,9 +1,55 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Contact } from '../model/contact';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
 
-  constructor() { }
+  private API_URL = '/api';
+
+  constructor(private http: HttpClient) { }
+
+
+  // get("/api/contacts")
+  getContacts(): Promise<void | Contact[]> {
+    return this.http.get(this.API_URL + "/users")
+      .toPromise()
+      .then(response => response as Contact[])
+      .catch(this.handleError);
+  }
+
+  // post("/api/contacts")
+  createContact(newContact: Contact): Promise<void | Contact> {
+    return this.http.post(this.API_URL + "/users", newContact)
+      .toPromise()
+      .then(response => response as Contact)
+      .catch(this.handleError);
+  }
+
+  // get("/api/contacts/:id") endpoint not used by Angular app
+
+  // delete("/api/contacts/:id")
+  deleteContact(delContactId: String): Promise<void | String> {
+    return this.http.delete(this.API_URL + "/users" + '/' + delContactId)
+      .toPromise()
+      .then(response => response as String)
+      .catch(this.handleError);
+  }
+
+  // put("/api/contacts/:id")
+  updateContact(putContact: Contact): Promise<void | Contact> {
+    var putUrl = this.API_URL + "/users" + '/' + putContact._id;
+    return this.http.put(putUrl, putContact)
+      .toPromise()
+      .then(response => response as Contact)
+      .catch(this.handleError);
+  }
+
+  private handleError(error: any) {
+    let errMsg = (error.message) ? error.message :
+      error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+    console.error(errMsg); // log to console instead
+  }
 }
